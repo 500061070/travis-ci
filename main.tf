9154482369f27a92dc10028f260148606e279efd
Rishabh Singh <35725297+500061070@users.noreply.github.com>
2021-04-30 02:38:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region  = "us-east-1" # Setting my region to London. Use your own region here
+}
+
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster" # Naming the cluster
 }
